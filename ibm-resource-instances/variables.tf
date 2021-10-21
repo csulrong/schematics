f0ea52574f4874c1fd5e30895ffa764dce49e95d
Rong Liang <jet.rongl@cn.ibm.com>
2021-10-21 14:54:58 +0800

    ibm cloud resource instances

---
+++ ibm-resource-instances/variables.tf
@@ -0,0 +1,21 @@
+variable "service_name" {
+  default = "friday-you-object-storage-jet"
+}
+
+variable "service_type" {
+  default = "cloud-object-storage"
+}
+
+variable "plan" {
+  default = "lite"
+}
+
+variable "location" {
+  default = "us-south"
+}
+
+variable "resource_group_name" {
+  default = "friday-you"
+}
+
+
